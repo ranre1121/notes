dd45355ae96f60173422e08cfdc996ef27a815cf
ranre1121 <pognar1121@gmail.com>
2025-09-04 17:41:31 +0500

    qeqeq

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+const App = () => {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100 text-xl font-bold">
+      Hello
+    </div>
+  );
+};
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import logger from "./middleware/logger.js";
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(cors());
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Logger middleware
+app.use(logger);
+
+//test
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Express backend!" });
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
